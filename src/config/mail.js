0f4bd5ffc848699eeb5e08a789ff7ab100bc4871
Gab360riel <gabrield_martins@hotmail.com>
2021-09-12 20:14:11 -0300

    estrutura e funcionalidade de envio de email ao criar e cancelar agendamento adicionada

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '735c921de2183a',
+    pass: '7d6633811c68fa',
+  },
+  default: {
+    from: 'Equipe FCalendar <noreply@fcalendar.com>',
+  },
+};
